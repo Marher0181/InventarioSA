f4e793949f09aae7bf0aae1b89b41ff345bea740
Marher0181 <marlonher0181@gmail.com>
2024-10-09 23:24:36 -0600

    Dashboards y usuarios

--- controllers/dashboards.py
+++ controllers/dashboards.py
@@ -29,4 +29,28 @@ def dashboardAdmin():
         categorias = db.session.execute(sql).fetchall()
         return render_template('DashboardAdmin.html', alertas=alertas, ventas=ventas, proveedores=proveedores, usuarios=usuarios, movimientos=movimientos, productos=productos, categorias=categorias)
 
-
+@dashboards_bp.route('/dashboardOperador')
+def dashboardOperador():
+    if 'usuarioSesion' not in session:
+        flash("Debes iniciar sesión para acceder a esta página.")
+        return redirect(url_for('login'))
+    
+    usuario = session['usuarioSesion']
+    idRol = usuario['idRol']
+    
+    if idRol == 1:
+        sql = db.text("SELECT COUNT(*) FROM Alertas")
+        alertas = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Ventas")
+        ventas = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Proveedores")
+        proveedores = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM MovimientosInventario")
+        movimientos = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Usuarios")
+        usuarios = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Productos")
+        productos = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Categorias")
+        categorias = db.session.execute(sql).fetchall()
+        return render_template('DashboardAdmin.html', alertas=alertas, ventas=ventas, proveedores=proveedores, usuarios=usuarios, movimientos=movimientos, productos=productos, categorias=categorias)
