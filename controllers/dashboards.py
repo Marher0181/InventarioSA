11ab6219b2d57ffad1dbc925650c4a68f6de9313
Marher0181 <marlonher0181@gmail.com>
2024-10-09 23:26:03 -0600

    Dashboards

--- controllers/dashboards.py
+++ controllers/dashboards.py
@@ -1,4 +1,4 @@
-from flask  import Flask, render_template, request, redirect, url_for, session, flash, send_file, Blueprint
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file, Blueprint
 from models import db, Categorias, Roles, Usuarios, Proveedores, Productos, MovimientosInventario, Ventas, Productos, DetalleVentas
 from sqlalchemy import text, func
 dashboards_bp = Blueprint('dashboards', __name__)
@@ -29,4 +29,26 @@ def dashboardAdmin():
         categorias = db.session.execute(sql).fetchall()
         return render_template('DashboardAdmin.html', alertas=alertas, ventas=ventas, proveedores=proveedores, usuarios=usuarios, movimientos=movimientos, productos=productos, categorias=categorias)
 
-
+@dashboards_bp.route('/dashboardOperador')
+def dashboardOperador():
+    if 'usuarioSesion' not in session:
+        flash("Debes iniciar sesión para acceder a esta página.")
+        return redirect(url_for('login'))
+    
+    usuario = session['usuarioSesion']
+    idRol = usuario['idRol']
+    
+    if idRol == 2:
+        sql = db.text("SELECT COUNT(*) FROM Ventas")
+        ventas = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Proveedores")
+        proveedores = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM MovimientosInventario")
+        movimientos = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Usuarios")
+        usuarios = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Productos")
+        productos = db.session.execute(sql).fetchall()
+        sql = db.text("SELECT COUNT(*) FROM Categorias")
+        categorias = db.session.execute(sql).fetchall()
+        return render_template('DashboardAdmin.html', ventas=ventas, proveedores=proveedores, usuarios=usuarios, movimientos=movimientos, productos=productos, categorias=categorias)
